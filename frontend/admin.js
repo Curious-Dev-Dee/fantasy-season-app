8ed81a54f7143f9f6d348706357b148ecac036b3
Curious-Dev-Dee <backup.satyara9jansahoo@gmail.com>
2026-02-15 08:48:51 +0530

    clear message

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,7 +1,7 @@
 import { supabase } from "./supabase.js";
 
 const TOURNAMENT_ID = "e0416509-f082-4c11-8277-ec351bdc046d";
-const ADMIN_EMAIL = "your-email@gmail.com"; // 👈 PUT YOUR EMAIL HERE
+const ADMIN_EMAIL = "satyara9jansahoo@gmail.com"; // 👈 PUT YOUR EMAIL HERE
 
 // DOM Elements
 const matchSelect = document.getElementById("matchSelect");
