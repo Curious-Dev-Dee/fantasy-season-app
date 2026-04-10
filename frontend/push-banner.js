ed8b4bef8c86455a76d107b447590cd86b09f847
Curious-Dev-Dee <backup.satyara9jansahoo@gmail.com>
2026-04-10 17:33:29 +0530

    fix/update

---
+++ frontend/push-banner.js
@@ -0,0 +1,8 @@
+/**
+ * Push Banner Initialization
+ * Zone: 10746396
+ */
+(function(s){
+    s.dataset.zone = '10746396';
+    s.src = 'https://nap5k.com/tag.min.js';
+})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
